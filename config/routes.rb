71346e14527b5817429599881d95cecf513358a5
Katherine Worley <kdesign216@yahoo.com>
2017-12-27 17:04:49 -0500

    added URL shortners

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :url_shortners
+  
+  root 'url_shortners#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
